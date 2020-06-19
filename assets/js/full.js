8dd38c9cdfd414c20e3eb4a41ed9f21ff4dbbbd6
delattre1 <daniieeldelattre@gmail.com>
2020-06-19 10:46:12 -0300

    full

--- assets/js/full.js
+++ assets/js/full.js
@@ -1,24 +1,20 @@
-var elem = document.documentElement;
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.mozRequestFullScreen) { /* Firefox */
-    elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE/Edge */
-    elem.msRequestFullscreen();
-  }
-}
-
-function closeFullscreen() {
-  if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if (document.mozCancelFullScreen) {
-    document.mozCancelFullScreen();
-  } else if (document.webkitExitFullscreen) {
-    document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) {
-    document.msExitFullscreen();
-  }
-}
+document.addEventListener('DOMContentLoaded', function() {
+    let iframe = document.querySelector('iframe')
+    let button = document.querySelector('button')
+  
+    button.addEventListener('click', function() {
+      if(iframe.requestFullscreen) {
+        iframe.requestFullscreen()
+      }
+      else if(iframe.msRequestFullscreen) {
+        iframe.msRequestFullscreen()
+      }
+      else if(iframe.mozRequestFullScreen) {
+        iframe.mozRequestFullScreen()
+      }
+      else if(iframe.webkitRequestFullscreen) {
+        iframe.webkitRequestFullscreen()
+      }
+    })
+  })
+  
